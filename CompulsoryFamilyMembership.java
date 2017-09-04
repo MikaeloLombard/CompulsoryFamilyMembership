e00cf54f4b79a24cba1407b38ac4b81731e69b5a
MiguelLopez <31246324+MikaeloLombard@users.noreply.github.com>
2017-09-04 19:56:04 -0400

    Add files via upload

---
+++ CompulsoryFamilyMembership.java
@@ -0,0 +1,40 @@
+/*
+ * Fun with challenge labs
+ */
+package compulsoryfamilymembership;
+
+import java.util.Scanner;
+
+
+public class CompulsoryFamilyMembership {
+
+   
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to the Compulsory Lopez Family Membership");
+        System.out.println("Warning:You are about to leave your family and become part of another.");
+        System.out.print("What is your name? ");
+        
+        String yourName = input.nextLine();
+        System.out.printf("Hello, %s!!!\n",yourName);
+        
+        System.out.print("Your name is now, ");
+        System.out.print(yourName);
+        String yourLastName;
+        yourLastName = " Lopez";
+        System.out.println(yourLastName);
+        System.out.println("Welcome to the Lopez family.");
+        System.out.println("If you were not Hispanic, you are now.");
+        System.out.println("Enter Chamber 2, to your right, to upload Salsa lessons to brain.");
+                
+     
+     
+        
+        
+        
+        // TODO code application logic here
+
+        // TODO code application logic here
+    }
+    
+}
